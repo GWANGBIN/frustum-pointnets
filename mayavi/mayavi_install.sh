79faf87cb8f517d653a39644a4d0f87fb6d630b3
Gwangbin Kim <GWANGBIN@users.noreply.github.com>
2018-11-02 14:55:08 +0900

    Update mayavi_install.sh

--- mayavi/mayavi_install.sh
+++ mayavi/mayavi_install.sh
@@ -2,4 +2,5 @@
 # Install Mayavi (scientific data visualization and plotting in Python) on Ubuntu
 # Ref: http://docs.enthought.com/mayavi/mayavi/installation.html
 sudo apt-get install python-vtk python-qt4 python-qt4-gl python-setuptools python-numpy python-configobj
+sudo pip install pyside
 sudo pip install mayavi
