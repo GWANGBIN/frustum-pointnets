5c054af95a730549531374a6d6f0e5d54277db0c
Gwangbin Kim <GWANGBIN@users.noreply.github.com>
2018-11-05 16:11:25 +0900

    Update and rename mayavi_install.sh to dependencies_install.sh

--- mayavi/dependencies_install.sh
+++ mayavi/dependencies_install.sh
@@ -1,6 +1,9 @@
 #/bin/bash
-# Install Mayavi (scientific data visualization and plotting in Python) on Ubuntu
+# Install dependent libraries
+#pdfcrop, gnuplot, pyside, mayavi(scientific data visualization and plotting in Python) on Ubuntu
 # Ref: http://docs.enthought.com/mayavi/mayavi/installation.html
 sudo apt-get install python-vtk python-qt4 python-qt4-gl python-setuptools python-numpy python-configobj
+sudo apt-get install texlive-extra-utils #pdfcrop
+sudo apt-get install gnuplot
 sudo pip install pyside
 sudo pip install mayavi
